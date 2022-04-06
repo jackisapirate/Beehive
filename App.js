e7dbad1d2dccc1341c97dcbba20ad0f24fb36cd5
jackisapirate <jackisapirate@example.com>
2022-04-05 23:23:33 -0700

    Add necessary dependencies

--- App.js
+++ App.js
@@ -6,15 +6,26 @@ import {
 } from "react-native";
 import React from "react";
 import Start from "./nav/Start";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Provider } from "mobx-react";
+import RootStore from "./mobx";
 
+const Stack = createNativeStackNavigator();
 const { width, height } = Dimensions.get("window");
 export default function App() {
   const state = { isShowLauncher: true };
   console.log(state.isShowLauncher);
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Start" >
+        <Stack.Screen name="Start" component={Start}></Stack.Screen>
+      </Stack.Navigator>
+    {/* <View style={styles.container}>
       <Start />
-    </View>
+    </View> */}
+    </NavigationContainer>
+
   );
 }
 
---
+++ utils/pathMap.js
@@ -0,0 +1,8 @@
+/**
+ * Back-end address + port
+ */
+export const BASE_URI = "http://this is a back-end uri";
+
+export const ACCOUNT_LOGIN = '/user/login'; // login: just one example
+
+export const ACCOUNT_REGISTER = '/user/register'; // register: just an example
